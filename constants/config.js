8f116954a7e661b57ca8fa99c2ce1e15c638f1fd
BhavikVora07 <bhavikbvorak56@gmail.com>
2024-07-05 13:19:01 +0530

    Changes in configuration File

--- constants/config.js
+++ constants/config.js
@@ -1,10 +1,11 @@
-export const allowedOrigins = ['http://localhost:5173', 'http://localhost:8080',"https://chat-app-6k7a.onrender.com","https://chat-app-frontend-puce-eight.vercel.app/"];
 const corsOptions = {
-  origin: allowedOrigins,
+  origin: '*',
   methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
 
 const CHATTU_TOKEN = "chattu-token";
 
 export { corsOptions, CHATTU_TOKEN };
+
