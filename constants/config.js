9ad702c2de3671c72ba80d5ffcd6b24c73c2323b
BhavikVora07 <bhavikbvorak56@gmail.com>
2024-07-05 12:47:37 +0530

    Changes in Config File

--- constants/config.js
+++ constants/config.js
@@ -1,4 +1,4 @@
-export const allowedOrigins = ['http://localhost:5173', 'http://localhost:8080'];
+export const allowedOrigins = ['http://localhost:5173', 'http://localhost:8080',"https://chat-app-6k7a.onrender.com","https://chat-app-frontend-puce-eight.vercel.app/"];
 const corsOptions = {
   origin: allowedOrigins,
   methods: ["GET", "POST", "PUT", "DELETE"],
