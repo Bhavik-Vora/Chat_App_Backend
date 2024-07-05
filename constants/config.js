da6ea9d98216a39ac4fc173769880168ff2c9359
BhavikVora07 <bhavikbvorak56@gmail.com>
2024-07-05 13:26:22 +0530

    Changes in configuration File

--- constants/config.js
+++ constants/config.js
@@ -1,11 +1,27 @@
+// constants/config.js
+
+export const allowedOrigins = [
+  'http://localhost:5173', 
+  'http://localhost:8080',
+  'https://chat-app-6k7a.onrender.com',
+  'https://chat-app-frontend-puce-eight.vercel.app',
+  'https://chat-app-6k7a.onrender.com/api/v1/user/login',
+  'https://chat-app-6k7a.onrender.com/api/v1/user/',
+  'https://chat-app-6k7a.onrender.com/api/v1/chat/',
+  'https://chat-app-6k7a.onrender.com/api/v1/admin/',
+];
+
 const corsOptions = {
-  origin: '*',
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-const CHATTU_TOKEN = "chattu-token";
-
-export { corsOptions, CHATTU_TOKEN };
-
+export { corsOptions };
